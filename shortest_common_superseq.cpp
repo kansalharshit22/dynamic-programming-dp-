8ea8d8c16eb5d01f90b26bf83aff4363355772ba
Harshit Kansal <hkansal_be23@thapar.edu>
2026-08-04 16:38:24 +0530

    Add bottom-up solution for shortest common supersequence
    Implemented a bottom-up dynamic programming approach to calculate the length of the shortest common supersequence of two strings.

--- shortest_common_superseq.cpp
+++ shortest_common_superseq.cpp
@@ -46,5 +46,36 @@ class Solution {
     }
 };
 
+//bottom up
+class Solution {
+public:
+    int minSuperSeq(string &s1, string &s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        
+        vector<vector<int>> t(n1 + 1, vector<int>(n2 + 1, 0));
+
+        // Base cases
+        for (int i = 0; i <= n1; i++) t[i][0] = i;
+        for (int j = 0; j <= n2; j++) t[0][j] = j;
+
+        // Fill table iteratively
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) { 
+              
+                if (s1[i - 1] == s2[j - 1]) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } 
+                else {
+                    t[i][j] = 1 + min(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+
+        return t[n1][n2];
+    }
+};
+
+
 //leetcode 1092
 //printing it
