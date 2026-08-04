bd56ad27b4cd5949650fd88a316363d93e49c199
Harshit Kansal <hkansal_be23@thapar.edu>
2026-08-04 16:59:17 +0530

    Implement shortest common supersequence function

--- shortest_common_superseq.cpp
+++ shortest_common_superseq.cpp
@@ -79,3 +79,60 @@ public:
 
 //leetcode 1092
 //printing it
+class Solution {
+public:
+    
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        
+        vector<vector<int>> t(n1 + 1, vector<int>(n2 + 1));
+        
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) { 
+                if(i==0||j==0)t[i][j]=i+j;
+                else if (s1[i - 1] == s2[j - 1]) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } 
+                else {
+                    t[i][j] = 1 + min(t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        
+
+        //printing form grid
+        string res="";
+        int i=n1, j=n2;
+
+        while(i && j){
+            if(s1[i-1]==s2[j-1]){
+                res.push_back(s1[i-1]);
+                i--;
+                j--;
+            }else{
+                if(t[i][j]>t[i-1][j]){
+                    //upr wala chota
+                    res.push_back(s1[i-1]);
+                    i--;
+                }else{
+                    //left move
+                    res.push_back(s2[j-1]);
+                    j--;
+                }
+            }
+        }
+        //push remaining
+        while(i>0){
+            res.push_back(s1[i-1]);
+            i--;
+        }
+        while(j){
+            res.push_back(s2[j-1]);
+            j--;
+        }
+
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
