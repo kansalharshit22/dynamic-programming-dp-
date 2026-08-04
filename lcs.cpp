f3d87b0fe4f15813c3514b37f684e00e8a46d1f2
Harshit Kansal <hkansal_be23@thapar.edu>
2026-08-04 17:27:37 +0530

    Add function to print LCS from two strings
    Implement function to print Longest Common Subsequence (LCS) using dynamic programming.

--- lcs.cpp
+++ lcs.cpp
@@ -47,3 +47,46 @@ public:
         return t[n1][n2];
     }
 };
+
+
+
+//printing lcs
+
+string printLCS(string text1, string text2) {
+    int n1 = text1.size();
+    int n2 = text2.size();
+
+    // 1. Build standard Bottom-Up DP Table
+    vector<vector<int>> t(n1 + 1, vector<int>(n2 + 1, 0));
+
+    for (int i = 1; i <= n1; i++) {
+        for (int j = 1; j <= n2; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                t[i][j] = 1 + t[i - 1][j - 1];
+            } else {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        }
+    }
+
+    // 2. Trace Back to Construct the LCS String
+    string lcs = "";
+    int i = n1, j = n2;
+
+    while (i > 0 && j > 0) {
+        if (text1[i - 1] == text2[j - 1]) {
+            lcs.push_back(text1[i - 1]); // Character is part of LCS
+            i--;
+            j--; // Move diagonally up-left
+        } else if (t[i - 1][j] > t[i][j - 1]) {
+            i--; // Move UP
+        } else {
+            j--; // Move LEFT
+        }
+    }
+
+    // 3. Reverse string as we built it backward
+    reverse(lcs.begin(), lcs.end());
+
+    return lcs;
+}
